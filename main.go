a4299aeb332f588ebe2af51f72594562ab984459
Kazuhiro Oinuma <oinume@gmail.com>
2019-11-24 07:16:32 +0900

    Update main

--- main.go
+++ main.go
@@ -3,31 +3,37 @@ package main
 import (
 	"context"
 	"log"
-	"time"
+	"net/http"
+	"net/http/httptrace"
 
 	"contrib.go.opencensus.io/exporter/zipkin"
 	open_zipkin "github.com/openzipkin/zipkin-go"
 	zipkin_http "github.com/openzipkin/zipkin-go/reporter/http"
+	"go.opencensus.io/plugin/ochttp"
 	"go.opencensus.io/trace"
 )
 
 func main() {
 	// TODO: http trace sample
-	exporter, flush, err := NewExporter("sample", true)
+	exporter, flush, err := NewExporter("sample")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer flush()
 	trace.RegisterExporter(exporter)
 
-	_, span := trace.StartSpan(context.Background(), "main")
+	ctx, span := trace.StartSpan(context.Background(), "main")
 	defer span.End()
-	time.Sleep(1 * time.Second)
+
+	if err := httpGet(ctx, "https://journal.lampetty.net/"); err != nil {
+		log.Fatal(err)
+	}
+	//time.Sleep(1 * time.Second)
 }
 
 type FlushFunc func()
 
-func NewExporter(service string, alwaysSample bool) (trace.Exporter, FlushFunc, error) {
+func NewExporter(service string) (trace.Exporter, FlushFunc, error) {
 	// 1. Configure exporter to export traces to Zipkin.
 	localEndpoint, err := open_zipkin.NewEndpoint(service, "192.168.1.5:5454")
 	if err != nil {
@@ -38,4 +44,21 @@ func NewExporter(service string, alwaysSample bool) (trace.Exporter, FlushFunc, 
 	exporter := zipkin.NewExporter(reporter, localEndpoint)
 	trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
 	return exporter, flush, nil
+}
+
+func httpGet(ctx context.Context, url string) error {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	ctx, span := trace.StartSpan(ctx, "httpGet")
+	defer span.End()
+	clientTrace := ochttp.NewSpanAnnotatingClientTrace(req, span)
+	ctx = httptrace.WithClientTrace(ctx, clientTrace)
+	req = req.WithContext(ctx)
+	_, err = http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	return nil
 }
