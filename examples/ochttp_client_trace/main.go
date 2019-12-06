210b4806bea5725aeacb0920d99fb2cdc8bf0a10
Kazuhiro Oinuma <oinume@gmail.com>
2019-12-06 16:36:04 +0900

    Add ochttp client trace example

---
+++ examples/ochttp_client_trace/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"net/http/httptrace"
+
+	"github.com/oinume/opencensus-client-trace-sample/opencensus"
+	"go.opencensus.io/plugin/ochttp"
+	"go.opencensus.io/trace"
+)
+
+func main() {
+	// TODO: Stackdriver exporter
+	exporter, flush, err := opencensus.NewExporter("sample")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer flush()
+	trace.RegisterExporter(exporter)
+
+	ctx, span := trace.StartSpan(context.Background(), "main")
+	defer span.End()
+
+	if err := httpGet(ctx, "https://journal.lampetty.net/"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func httpGet(ctx context.Context, url string) error {
+	ctx, span := trace.StartSpan(ctx, "httpGet")
+	defer span.End()
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	clientTrace := ochttp.NewSpanAnnotatingClientTrace(req, span)
+	ctx = httptrace.WithClientTrace(ctx, clientTrace)
+	req = req.WithContext(ctx)
+	_, err = http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	return nil
+}
