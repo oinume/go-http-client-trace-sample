07c861e5fd3b4b1ca79b2e8b161d21ac81207371
Kazuhiro Oinuma <oinume@gmail.com>
2022-07-09 11:01:42 +0900

    Add otelhttp_client_trace

---
+++ examples/otelhttp_client_trace/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"net/http/httptrace"
+
+	"go.opentelemetry.io/contrib/instrumentation/net/http/httptrace/otelhttptrace"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+
+	"github.com/oinume/opencensus-client-trace-sample/opentelemetry"
+)
+
+func main() {
+	tracerProvider, err := opentelemetry.NewTracerProvider("otelhttp_client_trace")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := tracerProvider.Shutdown(context.Background()); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	otel.SetTracerProvider(tracerProvider)
+
+	ctx := context.Background()
+	ctx, span := tracerProvider.Tracer("main").Start(ctx, "main")
+	defer span.End()
+
+	if err := httpGet(ctx, "https://journal.lampetty.net/"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func httpGet(ctx context.Context, url string) error {
+	ctx, span := otel.Tracer("main").Start(ctx, "httpGet")
+	defer span.End()
+	span.SetAttributes(attribute.KeyValue{
+		Key:   "url",
+		Value: attribute.StringValue(url),
+	})
+
+	clientTrace := otelhttptrace.NewClientTrace(ctx)
+	ctx = httptrace.WithClientTrace(ctx, clientTrace)
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return err
+	}
+	_, err = http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -7,9 +7,9 @@ require (
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/openzipkin/zipkin-go v0.4.0
 	go.opencensus.io v0.23.0
+	go.opentelemetry.io/contrib/instrumentation/net/http/httptrace/otelhttptrace v0.33.0
 	go.opentelemetry.io/otel v1.8.0
 	go.opentelemetry.io/otel/exporters/jaeger v1.8.0
-	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.8.0
 	go.opentelemetry.io/otel/sdk v1.8.0
 	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
 	google.golang.org/grpc v1.47.0 // indirect
