b0c546c5abc79ae838d2d33cd6d7b20bac6572f7
Kazuhiro Oinuma <oinume@gmail.com>
2022-07-09 09:09:57 +0900

    Use zipkin again

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/oinume/opencensus-client-trace-sample
 go 1.16
 
 require (
-	contrib.go.opencensus.io/exporter/jaeger v0.2.1
+	contrib.go.opencensus.io/exporter/zipkin v0.1.2
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/uber/jaeger-client-go v2.30.0+incompatible // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/openzipkin/zipkin-go v0.4.0
 	go.opencensus.io v0.23.0
-	google.golang.org/api v0.86.0 // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	google.golang.org/grpc v1.47.0 // indirect
 )
--- opencensus/opencensus.go
+++ opencensus/opencensus.go
@@ -1,25 +1,23 @@
 package opencensus
 
 import (
-	"contrib.go.opencensus.io/exporter/jaeger"
+	"contrib.go.opencensus.io/exporter/zipkin"
+	open_zipkin "github.com/openzipkin/zipkin-go"
+	zipkin_http "github.com/openzipkin/zipkin-go/reporter/http"
 	"go.opencensus.io/trace"
 )
 
 type FlushFunc func()
 
 func NewExporter(service string) (trace.Exporter, FlushFunc, error) {
-	// Port details: https://www.jaegertracing.io/docs/getting-started/
-	agentEndpointURI := "localhost:6831"
-	collectorEndpointURI := "http://localhost:14268/api/traces"
-
-	exporter, err := jaeger.NewExporter(jaeger.Options{
-		AgentEndpoint:     agentEndpointURI,
-		CollectorEndpoint: collectorEndpointURI,
-		ServiceName:       service,
-	})
+	// 1. Configure exporter to export traces to Zipkin.
+	localEndpoint, err := open_zipkin.NewEndpoint(service, "192.168.1.5:5454")
 	if err != nil {
 		return nil, nil, err
 	}
+	reporter := zipkin_http.NewReporter("http://localhost:9411/api/v2/spans")
+	flush := func() { _ = reporter.Close() }
+	exporter := zipkin.NewExporter(reporter, localEndpoint)
 	trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
-	return exporter, func() {}, nil
+	return exporter, flush, nil
 }
